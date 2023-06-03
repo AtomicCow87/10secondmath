946a147421f5a9c286dbbb0252c9fbe3aa5d8348
Zach <zkdoran@gmail.com>
2023-06-03 17:25:32 -0400

    rearranged

--- index.js
+++ index.js
@@ -1,65 +1,23 @@
 $(document).ready(function(){
   var currentQuestion;
-  var timeLeft = 10;
   var interval;
+  var timeLeft = 10;
   var score = 0;
-
+  
   var updateTimeLeft = function (amount) {
     timeLeft += amount;
     $('#time-left').text(timeLeft);
   };
-
-  var randomNumberGenerator = function (size) {
-    return Math.ceil(Math.random() * size);
-  };
-
-  var questionGenerator = function () {
-    var question = {};
-    var num1 = randomNumberGenerator(10);
-    var num2 = randomNumberGenerator(10);
-    
-    question.answer = num1 + num2;
-    question.equation = String(num1) + " + " + String(num2);
-    
-    return question;
-  };
-
-  var renderNewQuestion = function () {
-    currentQuestion = questionGenerator();
-    $('#equation').text(currentQuestion.equation);  
-  };
   
-  var checkAnswer = function (userInput, answer) {
-    if(userInput === answer) {
-      renderNewQuestion();
-      $('#user-input').val('');
-      updateTimeLeft(+1);
-    };
-  };
-  
-  $('#user-input').on('keyup', function () {
-    checkAnswer(Number($(this).val()), currentQuestion.answer);
-  });
-
   var updateScore = function (amount) {
     score += amount;
     $('#score').text(score);
   };
-
-  var checkAnswer = function (userInput, answer) {
-    if (userInput === answer) {
-      renderNewQuestion();
-      $('#user-input').val('');
-      updateTimeLeft(+1);
-      updateScore(+1);
-    }
-  };
-
+  
   var startGame = function () {
     if (!interval) {
       if (timeLeft === 0) {
         updateTimeLeft(10);
-        // Add the following
         updateScore(-score);
       }
       interval = setInterval(function () {
@@ -71,11 +29,40 @@ $(document).ready(function(){
       }, 1000);  
     }
   };
-
+  
+  var randomNumberGenerator = function (size) {
+    return Math.ceil(Math.random() * size);
+  };
+  
+  var questionGenerator = function () {
+    var question = {};
+    var num1 = randomNumberGenerator(10);
+    var num2 = randomNumberGenerator(10);
+    
+    question.answer = num1 + num2;
+    question.equation = String(num1) + " + " + String(num2);
+    
+    return question;
+  };
+  
+  var renderNewQuestion = function () {
+    currentQuestion = questionGenerator();
+    $('#equation').text(currentQuestion.equation);  
+  };
+  
+  var checkAnswer = function (userInput, answer) {
+    if (userInput === answer) {
+      renderNewQuestion();
+      $('#user-input').val('');
+      updateTimeLeft(+1);
+      updateScore(+1);
+    }
+  };
+  
   $('#user-input').on('keyup', function () {
-    startGame(); 
+    startGame();
     checkAnswer(Number($(this).val()), currentQuestion.answer);
   });
-
+  
   renderNewQuestion();
 });
