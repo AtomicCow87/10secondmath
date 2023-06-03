fb3b9ef68e3e8581916188d3b2220a96b170f95b
Zach <zkdoran@gmail.com>
2023-06-03 16:56:18 -0400

    adding quesiton generation to DOM

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-var randomNumberGenerator = function (size) {
-  return Math.ceil(Math.random() * size);
-}
+$(document).ready(function(){
+  var randomNumberGenerator = function (size) {
+    return Math.ceil(Math.random() * size);
+  }
 
-var questionGenerator = function () {
-  var question = {};
-  var num1 = randomNumberGenerator(10);
-  var num2 = randomNumberGenerator(10);
-  
-  question.answer = num1 + num2;
-  question.equation = String(num1) + " + " + String(num2);
-  
-  return question;
-}
+  var questionGenerator = function () {
+    var question = {};
+    var num1 = randomNumberGenerator(10);
+    var num2 = randomNumberGenerator(10);
+    
+    question.answer = num1 + num2;
+    question.equation = String(num1) + " + " + String(num2);
+    
+    return question;
+  }
+
+  currentQuestion = questionGenerator();
+  $('#equation').text(currentQuestion.equation);
+});
